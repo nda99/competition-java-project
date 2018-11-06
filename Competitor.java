2c5ef4d9557e7975f228e0ff9b32d636855d56d5
aat4 <44607697+aat4@users.noreply.github.com>
2018-11-06 11:25:12 +0000

    Add files via upload

---
+++ Competitor.java
@@ -0,0 +1,129 @@
+
+public abstract class Competitor {
+	
+	private int competitorNumber;
+	private Name competitorName;
+	private int[] score;
+	private String level;
+	
+	
+	
+	/** Constructs a competitor
+	 * 
+	 * @param competitorNumber Int containing the number of the competitor
+	 * @param competitorName Name object containing the name details of competitor
+	 * @param score Int array containing the scores of the competitor
+	 * @param level String of the competitor's level
+	 * @see Competitor
+	 */
+	public Competitor (int competitorNumber, Name competitorName, int[] score, String level)
+	{
+		this.competitorNumber = competitorNumber;
+		this.competitorName = competitorName;
+		this.level = level;
+	}
+	
+	
+	
+	/**
+	 *  Calculates overall score for the competitor based on each score and level.
+	 * @see getOverallScore 
+	 * @see Competitor
+	 * @return Overall score obtained by competitor
+	 */
+	
+	public abstract double getOverallScore();
+	
+	/** Simple method to generate string for full details of the competitor
+	 * @see getFullDetails
+	 * @return String with full details of the competitor
+	 */
+	public abstract String getFullDetails() ;
+	
+	
+
+	/** Simple method to generate short details of the competitor
+	 * 
+	 * @return String with short details of the competitor
+	 */
+	public String getShortDetails() 
+	{
+		
+		return String.format("CN %d (%s) has overall score %1.1f.\n", competitorNumber, competitorName.getInitials(), getOverallScore());
+	}
+	
+	
+	/** Simple method to get the scores of the competitor
+	 * 
+	 * @return int [] array with all scores of the competitor
+	 */
+	public int[] getScoreArray() 
+	{
+		return score;
+	}
+	
+	/** Simple method to get the name of the competitor name for report
+	 * 
+	 * @return String with full name of a competitor
+	 */
+	public String getCompetitorName() 
+	{
+	
+		return competitorName.getFullName();
+	}
+	
+	
+	/** Simple method to get the level of the competitor 
+	 * 
+	 * @return String with level of competitor
+	 */
+	public String getLevel() 
+	{
+		return level;
+	}
+	
+	/** Simple method to get the number of the competitor 
+	 * 
+	 * @return Integer containing the competitor's number
+	 */
+	public int getCompetitorNumber() 
+	{
+		return competitorNumber;
+	}
+
+
+	/** Simple method to set the number of the competitor 
+	 * 
+	 */
+	public void setCompetitorNumber(int competitorNumber) {
+		this.competitorNumber = competitorNumber;
+	}
+
+
+	/** Simple method to set the name of the competitor 
+	 * 
+	 */
+	public void setCompetitorName(Name competitorName) {
+		this.competitorName = competitorName;
+	}
+
+
+	/** Simple method to set the scorea of the competitor 
+	 * 
+	 */
+	public void setScore(int[] score) {
+		this.score = score;
+	}
+
+
+	/** Simple method to set the level of the competitor 
+	 * 
+	 */
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	
+	
+	
+
+}
--- CompetitorList.java
+++ CompetitorList.java
@@ -1,41 +1,37 @@
-package ndaAssignment1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Class to hold and handle details and statistics of an ArrayList of HaggisChef competitors. 
- * Performs operations on the list, but not on individual competitors.
- * @author Nathan
- * @see ndaAssignment1.HaggisChef
- * @see ndaAssignment1.Manager
- */
-public class CompetitorList {
-
-	private ArrayList<HaggisChef> competitors = new ArrayList<HaggisChef>();
-
+public abstract class CompetitorList {
+	
+	
+	private ArrayList<Competitor> competitors = new ArrayList<Competitor>();
+	
 	/**
 	 * Constructor for Competitor List object, which holds a list of all competitors.
-	 * @param competitor is an ArrayList of all the competitor objects (HaggisChef).
+	 * @param competitor is an ArrayList of all the competitor objects (Competitor).
 	 */
-	public CompetitorList(ArrayList<HaggisChef> competitor) {
+	public CompetitorList(ArrayList<Competitor> competitor) 
+	{
 		this.competitors = competitor;
+	
 	}
 	
+
 	/**
 	 * Method to get a competitor of choice based on his/her Competitor Number (CN)
 	 * @param CN competitor number
-	 * @return HaggisChef object holding the information of the competitor of choice.
+	 * @return Competitor object holding the information of the competitor of choice.
 	 * @see CompetitorList
-	 * @see HaggisChef 
+	 * @see Competitor 
 	 */
-	public HaggisChef getCompetitor(int CN) {
+	public Competitor getCompetitor(int CN) {
 
-		HaggisChef choice = null;
+		Competitor choice = null;
 
-		for (HaggisChef h : this.competitors) {
-			if (h.getCN() == CN) {
-				choice = h;
+		for (Competitor c : this.competitors) {
+			if (c.getCompetitorNumber() == CN) {
+				choice = c;
 				break;
 			}
 		}
@@ -46,52 +42,27 @@ public class CompetitorList {
 
 		return choice;
 
-	}
-
+}
+	
     /**
      * Method which loops through list to find competitor with HIGHEST SCORE.
-     * @return HaggisChef object holding the details of the competitor with highest score.
-     * @see ndaAssignment1.HaggisChef
-     * @see ndaAssignment1.CompetitorList
+     * @return Competitor object holding the details of the competitor with highest score.
      */
-	public HaggisChef getBestCompetitor() {
+	
+	public Competitor getBestCompetitor() {
 		double score = -1;
-		HaggisChef best = null;
-		for (HaggisChef h : this.competitors) {
-			if (h.getOverallScore() > score) {
-				score = h.getOverallScore();
-				best = h;
+		Competitor best = null;
+		for (Competitor c : this.competitors) {
+			if (c.getOverallScore() > score) {
+				score = c.getOverallScore();
+				best = c;
 			}
 		}
 
 		return best;
 	}
-
-	/**
-	 * Method to build the report based on the list of competitors.
-	 * @return String variable containing report information based on the list.
-	 * @see ndaAssignment1.Manager
-	 * @see getFreq
-	 * @see getStats
-	 */
-	public String getReport() {
-		String rep = String.format("--------\nREPORT:\n%20.50s %18.10s %18.30s %26.6s %15.8s\n", "Competitor", "Level", "Dish", "Scores",
-				"Overall");
-
-		for (HaggisChef h : competitors) {
-
-			// rep = String.format(rep + "%1.20s %10.10s %2.5d %2.5d %2.5d %2.5d %2.5d
-			// %10.8f\n",h.getName(),h.getLevel(),h.getScoreArray()[0],h.getScoreArray()[1],h.getScoreArray()[2],h.getScoreArray()[3],h.getScoreArray()[4],h.getOverallScore());
-
-			rep = String.format(rep + "%3.3s %25.25s %10.10s %31.31s %15.20s %10.1f\n", h.getCN(), h.getName(),
-					h.getLevel(), h.getDish(),
-					Arrays.toString(h.getScoreArray()).replace("[", "").replace("]", "").replace(",", " "),
-					h.getOverallScore());
-
-		}
-		return rep;
-	}
-
+	
+	
 	 /**
 	  * Method to compute Frequency of scores: [ 0 1 2 3 4 5] 
 	  * @return int[] array with frequency of each score on the list
@@ -99,31 +70,31 @@ public class CompetitorList {
 	public int[] getFreq() {
 		int[] freq = new int[6];
 
-		for (HaggisChef h : competitors) {
+		for (Competitor c : competitors) {
 			for (int i = 0; i < 5; i++) {
-				if (h.getScoreArray()[i] == 0) {
+				if (c.getScoreArray()[i] == 0) {
 					freq[0]++;
-				} else if (h.getScoreArray()[i] == 1) {
+				} else if (c.getScoreArray()[i] == 1) {
 					freq[1]++;
-				} else if (h.getScoreArray()[i] == 2) {
+				} else if (c.getScoreArray()[i] == 2) {
 					freq[2]++;
-				} else if (h.getScoreArray()[i] == 3) {
+				} else if (c.getScoreArray()[i] == 3) {
 					freq[3]++;
-				} else if (h.getScoreArray()[i] == 3) {
+				} else if (c.getScoreArray()[i] == 3) {
 					freq[3]++;
-				} else if (h.getScoreArray()[i] == 4) {
+				} else if (c.getScoreArray()[i] == 4) {
 					freq[4]++;
-				} else if (h.getScoreArray()[i] == 5) {
+				} else if (c.getScoreArray()[i] == 5) {
 					freq[5]++;
 				}
 			}
 		}
 
 		return freq;
-	}
+}
 	
 	/**
-	 * Method to get STATISTICS for the Haggis Competition.
+	 * Method to get STATISTICS for the Competition.
 	 * @return String variable holding statistical details of the competition.
 	 */
 	public String getStats() {
@@ -136,39 +107,43 @@ public class CompetitorList {
 		total = competitors.size();
 		
 		//get Average score;		
-		for (HaggisChef h : competitors) {
+		for (Competitor h : competitors) {
 			avgScore += h.getOverallScore();
 		}
 		avgScore /= total;
 		
-		//get number of competitors
-		
-		int masterChef = 0, amateur = 0, cook = 0, chef = 0;
-		for (HaggisChef c : competitors) {
-			switch (c.getLevel()){
-			case "MasterChef": masterChef++;
-			break;
-			case "Chef": chef++;
-			break;
-			case "Cook": cook++;
-			break;
-			case "Amateur": amateur++;
-			break;
-			}
-		}
-		
+			
 		//get Max score
 		maxScore = getBestCompetitor().getOverallScore();
-		winner = getBestCompetitor().getName();
+		winner = getBestCompetitor().getCompetitorName();
 		//getMinScore
-		for (HaggisChef h : competitors) {
-			if (h.getOverallScore() < minScore) {
-				minScore = h.getOverallScore();
+		for (Competitor c : competitors) {
+			if (c.getOverallScore() < minScore) {
+				minScore = c.getOverallScore();
 			}
 		}
 			
-		String stats = String.format("-------------\nSTATISTICS:\nWinner of Haggis Competition is %s with an overall score of %1.1f \nTotal number of competitors: %d, Min score: %1.1f, Max score: %1.1f, Avg score: %1.1f\nScore:     0  1   2   3   4   5\nFrequency: %s\nAmateurs: %d, Cooks: %d, Chefs: %d, Master Chefs: %d", winner, maxScore, total, minScore, maxScore, avgScore, Arrays.toString(getFreq()).replace("[", "").replace("]", "").replaceAll(","," "),amateur,cook,chef,masterChef);
+		String stats = String.format("-------------\nSTATISTICS:\nWinner of "
+				+ "Haggis Competition is %s with an overall score of %1.1f \nTotal number of competitors: "
+				+ "%d, Min score: %1.1f, Max score: %1.1f, Avg score: %1.1f\nScore:"
+				+ "     0  1   2   3   4   5\nFrequency: %s", 
+				winner, maxScore, total, minScore, maxScore, avgScore, Arrays.toString(getFreq()).replace("[", "").replace("]", "").replaceAll(","," "),getLevelFreq());
 		
 		return stats;
-	}
+}
+	
+	/**
+	 * Method to build the report based on the list of competitors.
+	 * @return String variable containing report information based on the list.
+	 * @see getFreq
+	 * @see getStats
+	 */
+public abstract String getReport();
+
+/**
+ * Method to get frequency of competitor levels.
+ */
+public abstract String getLevelFreq();
+
+	
 }
